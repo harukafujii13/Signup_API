d7aae5dea12101f15b1241a1d56e2ec87cd09db7
Haruka <hfujii1013@gmail.com>
2023-03-15 23:48:57 -0700

    added public

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function(){
-  console.log("Server is running on port 3000")
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
