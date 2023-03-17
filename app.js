0f8eae72cd279d7ffb1339f64d42436467096b1b
Haruka <hfujii1013@gmail.com>
2023-03-16 18:45:08 -0700

    added API

--- app.js
+++ app.js
@@ -3,15 +3,59 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("http");
 
 const app = express();
+
 app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us18.api.mailchimp.com/3.0/lists/f29c460a52.";
+
+  const options = {
+    method: "POST",
+    auth: "haruka1 : aed1476c1369d6ec2d8a8c480ee2a9c0-us18",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
+
+//API Key
+// aed1476c1369d6ec2d8a8c480ee2a9c0-us18
+
+//List Id
+// f29c460a52.
